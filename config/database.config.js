6e3c18574d27cf6988d7a61d67eb4f138a5f7ae6
Razi <razibhatti1@gmail.com>
2019-11-02 00:20:52 +0500

    basic MVC

---
+++ config/database.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    url: 'mongodb+srv://l145011:mongo123@cluster0-bbl6l.mongodb.net/test?retryWrites=true&w=majority'
+}
+/*
+module.exports = {
+    url: 'mongodb://localhost:27017/easy-notes'
+}*/
